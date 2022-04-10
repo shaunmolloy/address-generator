e217110cbfddccabc60af0e5528de0da983ac6c0
Shaun Molloy <shaun.molloy@loan.co.uk>
2022-04-10 19:05:55 +0100

    Add data files for streets, towns & postcodes

---
+++ src/data/streets.js
@@ -0,0 +1,13 @@
+module.exports = [
+    'Bury Road',
+    'Bury New Road',
+    'Bury Old Road',
+    'Chorley Road',
+    'Chorley New Road',
+    'Chorley Old Road',
+    'Highfield Road',
+    'Huddersfield Road',
+    'Lancashire Road',
+    'Manchester Road',
+    'Wigan Road',
+];
